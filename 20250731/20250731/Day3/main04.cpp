7abf9d004afffc22c15349d3363d1be6f89821c8
sbscerti <160083857+sbscerti@users.noreply.github.com>
2025-08-04 15:14:59 +0900

    수업내용커밋

---
+++ 20250731/20250731/Day3/main04.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+int main()
+{
+	// ------------------------------------
+	//  3. 조건문 (if - else if - else)
+	// ------------------------------------
+
+	// if ( A ) 
+	// {
+	//		본문1
+	// } 
+	// else 
+	// {
+	//		본문2
+	// }
+
+	// if ( A ) 
+	// {
+	//		본문1
+	// } 
+	// else if ( B )
+	// {
+	//		본문2
+	// }
+	// else 
+	// {
+	//		본문3
+	// }
+	// 
+	// =>  A 가 참이면 
+	//			본문1
+	//     그렇지 않고, B가 참이면
+	//			본문2
+	//	   그렇지 않으면
+	//			본문3
+
+	// 예제
+	int score = 0;
+	// print(성적을 입력해주세요)
+	// input score
+	// if ( 90 <= score ) then
+	//		print(A 입니다)
+	// else if ( 80 <= score ) then
+	//		print(B 입니다)
+	// else if ( 70 <= score ) then
+	//		print(C 입니다)
+	// else if ( 60 <= score ) then
+	//		print(D 입니다)
+	// else 
+	//		print(F 입니다)
+
+	printf("성적을 입력해주세요 : ");
+	scanf_s("%d", &score);
+	if (90 <= score)
+	{
+		printf("A 입니다.\n");
+	}
+	else if (80 <= score)
+	{
+		printf("B 입니다.\n");
+	}
+	else if (70 <= score)
+	{
+		printf("C 입니다.\n");
+	}
+	else if (60 <= score)
+	{
+		printf("D 입니다.\n");
+	}
+	else
+	{
+		printf("F 입니다.\n");
+	}
+
+	// 당분간 실행은 F11로 하겠습니다.
+	// F11은 int main함수의 가장 첫번째줄부터 실행하는 실행단축키입니다.
+
+}
---
+++ 20250731/20250731/Day3/main05.cpp
@@ -0,0 +1,155 @@
+﻿#include <iostream>
+
+int main()
+{
+	// ------------------------------------
+	//  4. switch - case 문
+	// ------------------------------------
+
+	// 여러가지 선택지 중에서
+	// "일치하는 항목"을 딱 골라서 실행해줌
+
+	// 예제
+	int input = 0;
+	printf("자판기 번호를 골라주세요.");
+	scanf_s("%d", &input);
+
+	switch (input)
+	{
+	case 1:
+		printf("콜라\n");
+		break;
+	case 2:
+		printf("사이다\n");
+		break;
+	case 3:
+		printf("환타\n");
+		break;
+	default:
+		printf("잘못된 입력입니다.\n");
+		break;
+	}
+
+	if (input == 1)
+	{
+		printf("콜라\n");
+	}
+	else if (input == 2)
+	{
+		printf("사이다\n");
+	}
+	else if (input == 3)
+	{
+		printf("환타\n");
+	}
+	else
+	{
+		printf("잘못된 입력입니다.\n");
+	}
+
+	/*
+	* 암기할 부분
+	switch (값)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	default:
+		break;
+	}
+	*/
+
+	// ------------------------------------
+	//  5. 변수의 유효범위 
+	// ------------------------------------
+
+	int a = 3;	// *어떠한 경우에서도 a, b, c 변수명은 되게 안좋습니다.
+
+	// 변수는 언제 삭제 되냐.
+	// 스코프가 끝나면 삭제됩니다.
+	// } 중괄호가 끝나면 삭제됩니다.
+
+	{
+		// b라는 변수 생성
+		int b = 0;
+	}
+
+	// b라는 변수 생성 (위의 b랑은 다름) (이름만 같음)
+	int b = 0;
+	/*
+	switch (값)
+	{
+	case 1:
+	{
+		int a = 3;
+		break;
+	}
+	case 2:
+	{
+		int a = 3;
+		break;
+	}
+	case 3:
+		break;
+	default:
+		break;
+	}
+	*/
+
+
+	switch (input)
+	{
+		case 1:
+		{
+			printf("콜라를 선택했습니다.\n 어떤 콜라를 원하시나요\n 1: 코카콜라, 2: 펩시");
+			int input2 = 0;
+			scanf_s("%d\n", &input2);
+			switch (input2)
+			{
+			case 1:
+				printf("코카콜라\n");
+				break;
+			case 2:
+				printf("펩시\n");
+				break;
+			default:
+				printf("잘못 골랐습니다.\n");
+				break;
+			}
+			break;
+		}
+		case 2:
+		{
+			printf("사이다를 선택했습니다.\n 어떤 사이다를 원하시나요\n 1: 칠성사이다, 2: 스프라이트");
+			int input2 = 0;
+			scanf_s("%d\n", &input2);
+			switch (input2)
+			{
+			case 1:
+				printf("칠성사이다\n");
+				break;
+			case 2:
+				printf("스프라이트\n");
+				break;
+			default:
+				printf("잘못 골랐습니다.\n");
+				break;
+			}
+			break;
+		}
+		case 3:
+		{
+			printf("환타\n");
+			break;
+		}
+		default:
+		{
+			printf("잘못된 입력입니다.\n");
+			break;
+		}
+	}
+
+}
